1948b093a62902118ea6e881b0fca08854d2c218
Sarah-Specialist <92344379+Sarah-Specialist@users.noreply.github.com>
2021-12-21 13:06:50 +0800

    updated opacity

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
+function menu() {
+    const toggleButton = document.getElementsByClassName('toggle-button')[0]
+    const hamburgerLinks = document.getElementsByClassName('navbar')[0]
+    console.log("hamburger clicked")
+    hamburgerLinks.classList.toggle('active');
+}
+
+/*
 const toggleButton = document.getElementsByClassName('toggle-button')[0]
-const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+const hamburgerLinks = document.getElementsByClassName('hamburger')[0]
 
 toggleButton.addEventListener('click', () => {
-    navbarLinks.classList.toggle('active');
+    hamburgerLinks.classList.toggle('active')
 })
+*/

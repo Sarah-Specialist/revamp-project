33c99edcb279ef0dac5e84e791bc3cdfcdf8f92f
Sarah-Specialist <92344379+Sarah-Specialist@users.noreply.github.com>
2021-12-23 07:27:00 +0800

    Updated to working JS for now

--- index/index.js
+++ index/index.js
@@ -2,12 +2,12 @@ function menu() {
     //const toggleButton = document.getElementsByClassName('toggle-button')[0]
     const navbar = document.getElementsByClassName('navbar')[0]
     console.log("hamburger clicked")
-    //navbar.style.display="flex";
+    navbar.style.display="flex";
 
     
-    if (navbar.style.display=='flex') {
+    /*if (navbar.style.display=='flex') {
         navbar.style.display='none';
     } else if (navbar.style.display=='none') {
         navbar.style.display='flex';
-    }
+    }*/
 }

3c6b3f58edcd082b703581508a268ebb28ac1d5f
Sarah Specialist <sarah.specialist@gmail.com>
2021-12-27 20:25:08 +0800

    added submit button

--- contact/index.js
+++ contact/index.js
@@ -2,9 +2,17 @@ const toggleButton = document.getElementByClassName('toggle-button');
 
 document.getElementByClassName('toggle-button').addEventListener('click', function(evt) {
   let navbar = document.getElementById(navbar-links);
-  if (navbar.style.display === 'flex') {
+  if (toggleButton.clicked) {
     navbar.style.display = 'none';
-  } else if (navbar.style.display === 'none') {
+  } else if (!toggleButton.clicked) {
     navbar.style.backgroundColor = 'flex';
   }
 }, false);
+
+const numberFormat = ^[89]\d{7}$;
+const phoneNumber = document.getElementById("number");
+if (phoneNumber !== numberFormat) {
+  alert "Please enter a valid phone number";
+}
+
+// Unable to find concensus on email validation, hence left out for this project
